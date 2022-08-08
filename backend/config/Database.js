a81cc8e8962916ae3dd8362a07f8d062779871d5
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2022-08-08 14:14:18 +0600

    remove code

--- backend/config/Database.js
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from "sequelize";
-
-const db = new Sequelize('nploy_native','root','',{
-    host: 'localhost',
-    port: '3306',
-    dialect: "mysql"
-});
-
-export default db;
